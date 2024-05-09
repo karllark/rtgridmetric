e0685ffde9f8eae7964ba4e71fa2d31c6146e0b4
Karl D. Gordon <kgordon@stsci.edu>
2024-05-09 16:43:19 -0400

    analyze for "multi wave" run - easier

--- analyze_unctest.py
+++ analyze_unctest.py
@@ -3,16 +3,13 @@ from astropy.io import fits
 
 if __name__ == "__main__":
     basefile = "output_unctest/dirty_gridmetric_slab_tau1_10_10_10"
-    # get the radiation fields from 100 runs and compute an empirical unc
-    nfiles = 100
-    for i in range(nfiles):
-        cfile = f"{basefile}_n{i+1}_rad_field.fits"
-        cdata = fits.getdata(cfile)
-        if i == 0:
-            adata = np.zeros((cdata.shape[1], cdata.shape[2], cdata.shape[3], nfiles))
-        adata[:, :, :, i] = cdata[0, :, :, :]
 
-    # compute the std in each cell
-    dataunc = np.std(adata, axis=3)
+    # empirical unc
+    adata = fits.getdata(f"{basefile}_rad_field.fits")
+    dataunc = np.std(adata, axis=0)
+    fits.writeto(f"{basefile}_rad_field_empirunc.fits", dataunc, overwrite=True)
 
-    fits.writeto(f"{basefile}_rad_field_empirunc.fits", dataunc)
+    # average calcuated unc
+    adata = fits.getdata(f"{basefile}_rad_field_unc.fits")
+    dataunc = np.average(adata, axis=0)
+    fits.writeto(f"{basefile}_rad_field_aveunc.fits", dataunc, overwrite=True)
