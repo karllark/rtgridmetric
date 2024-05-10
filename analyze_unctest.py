74579e20b96df4bfbf8691741431373267e422f2
Karl D. Gordon <kgordon@stsci.edu>
2024-05-10 17:30:51 -0400

    minor update

--- analyze_unctest.py
+++ analyze_unctest.py
@@ -2,11 +2,11 @@ import numpy as np
 from astropy.io import fits
 
 if __name__ == "__main__":
-    basefile = "output_unctest/dirty_gridmetric_slab_tau1_10_10_10"
+    basefile = "output_unctest/dirty_gridmetric_slab_tau1_10_10_10_1e5"
 
     # empirical unc
     adata = fits.getdata(f"{basefile}_rad_field.fits")
-    dataunc = np.std(adata, axis=0)
+    dataunc = np.std(adata, axis=0, ddof=1)
     fits.writeto(f"{basefile}_rad_field_empirunc.fits", dataunc, overwrite=True)
 
     # average calcuated unc
