4decb66384aa119565d2a404debf6858ca4483ad
Karl Gordon <kgordon@stsci.edu>
2025-10-24 15:25:03 -0400

    adding option to view a 2nd slice

--- plotting/plot_grid_slice.py
+++ plotting/plot_grid_slice.py
@@ -9,13 +9,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("basename", help="base name for the grid")
     parser.add_argument("--xval", default=0.0, type=float, help="x slice value")
-    parser.add_argument("--zval", default=-2.5, type=float, help="z slice value")
+    parser.add_argument("--zval", default=-3.5, type=float, help="z slice value")
+    parser.add_argument("--xval2", default=0.0, type=float, help="x slice value")
+    parser.add_argument("--zval2", default=-3.5, type=float, help="z slice value")
     parser.add_argument("--png", help="save figure as a png file", action="store_true")
     parser.add_argument("--pdf", help="save figure as a pdf file", action="store_true")
     args = parser.parse_args()
 
     xval = args.xval
     zval = args.zval
+    xval2 = args.xval2
+    zval2 = args.zval2
 
     posfile = fits.open(f"{args.basename}_pos.fits")
     taufile = fits.open(f"{args.basename}_tau_ref_per_pc.fits")
@@ -35,7 +39,10 @@ if __name__ == "__main__":
 
     parent_grid = True
     cur_ptype = "k-"
+    cur_ptype2 = "r-"
     cur_alpha = 1.0
+    cur_zorder = 2000
+    cur_zorder2 = 1000
     for hpos, htau in zip(posfile, taufile):
         pos = hpos.data
         # transpose to translate between C++ and python
@@ -46,17 +53,19 @@ if __name__ == "__main__":
         zsize = tau.shape[2]
 
         # xy slice
-        if (zval >= np.min(pos[2, 0 : zsize + 1])) & (zval < np.max(pos[2, 0 : zsize + 1])):
+        if (zval >= np.min(pos[2, 0 : zsize + 1])) & (
+            zval < np.max(pos[2, 0 : zsize + 1])
+        ):
             ax = axs[0]
             yvals = [np.min(pos[1, 0 : ysize + 1]), np.max(pos[1, 0 : ysize + 1])]
             for i in range(xsize + 1):
                 xvals = pos[0, i] * np.array([1.0, 1.0])
-                ax.plot(xvals, yvals, cur_ptype, alpha=cur_alpha)
+                ax.plot(xvals, yvals, cur_ptype, alpha=cur_alpha, zorder=cur_zorder)
 
             xvals = [np.min(pos[0, 0 : xsize + 1]), np.max(pos[0, 0 : xsize + 1])]
             for j in range(ysize + 1):
                 yvals = pos[1, j] * np.array([1.0, 1.0])
-                ax.plot(xvals, yvals, cur_ptype, alpha=cur_alpha)
+                ax.plot(xvals, yvals, cur_ptype, alpha=cur_alpha, zorder=cur_zorder)
 
         if parent_grid:
             ax.set_xlim([np.min(pos[0, 0 : xsize + 1]), np.max(pos[0, 0 : xsize + 1])])
@@ -65,27 +74,79 @@ if __name__ == "__main__":
             ax.set_ylabel("y [pc]")
             ax.set_title(f"z = {zval:.2f} pc")
 
+        # xy slice #2
+        if (
+            (not parent_grid)
+            & (zval != zval2)
+            & (zval2 >= np.min(pos[2, 0 : zsize + 1]))
+            & (zval2 < np.max(pos[2, 0 : zsize + 1]))
+        ):
+            ax = axs[0]
+            yvals = [np.min(pos[1, 0 : ysize + 1]), np.max(pos[1, 0 : ysize + 1])]
+            for i in range(xsize + 1):
+                xvals = pos[0, i] * np.array([1.0, 1.0])
+                ax.plot(xvals, yvals, cur_ptype2, alpha=cur_alpha, zorder=cur_zorder2)
+
+            xvals = [np.min(pos[0, 0 : xsize + 1]), np.max(pos[0, 0 : xsize + 1])]
+            for j in range(ysize + 1):
+                yvals = pos[1, j] * np.array([1.0, 1.0])
+                ax.plot(xvals, yvals, cur_ptype2, alpha=cur_alpha, zorder=cur_zorder2)
+
+        if parent_grid:
+            ax.set_xlim([np.min(pos[0, 0 : xsize + 1]), np.max(pos[0, 0 : xsize + 1])])
+            ax.set_ylim([np.min(pos[1, 0 : ysize + 1]), np.max(pos[1, 0 : ysize + 1])])
+            ax.set_xlabel("x [pc]")
+            ax.set_ylabel("y [pc]")
+            ktitle = rf"z = {zval:.2f} pc"
+            if zval2 != zval:
+                ktitle = f"{ktitle}; z2 = {zval2:.2f} pc"
+            ax.set_title(ktitle)
+
         # yz slice
-        if (xval >= np.min(pos[0, 0 : xsize + 1])) & (xval < np.max(pos[0, 0 : xsize + 1])):
+        if (xval >= np.min(pos[0, 0 : xsize + 1])) & (
+            xval < np.max(pos[0, 0 : xsize + 1])
+        ):
             ax = axs[1]
             yvals = [np.min(pos[1, 0 : ysize + 1]), np.max(pos[1, 0 : ysize + 1])]
             for i in range(zsize + 1):
                 zvals = pos[2, i] * np.array([1.0, 1.0])
-                ax.plot(zvals, yvals, cur_ptype, alpha=cur_alpha)
+                ax.plot(zvals, yvals, cur_ptype, alpha=cur_alpha, zorder=cur_zorder)
 
             zvals = [np.min(pos[2, 0 : zsize + 1]), np.max(pos[2, 0 : zsize + 1])]
             for j in range(ysize + 1):
                 yvals = pos[1, j] * np.array([1.0, 1.0])
-                ax.plot(zvals, yvals, cur_ptype, alpha=cur_alpha)
+                ax.plot(zvals, yvals, cur_ptype, alpha=cur_alpha, zorder=cur_zorder)
+
+        # yz slice #2
+        if (
+            (not parent_grid)
+            & (xval != xval2)
+            & (xval2 >= np.min(pos[0, 0 : xsize + 1]))
+            & (xval2 < np.max(pos[0, 0 : xsize + 1]))
+        ):
+            ax = axs[1]
+            yvals = [np.min(pos[1, 0 : ysize + 1]), np.max(pos[1, 0 : ysize + 1])]
+            for i in range(zsize + 1):
+                zvals = pos[2, i] * np.array([1.0, 1.0])
+                ax.plot(zvals, yvals, cur_ptype2, alpha=cur_alpha, zorder=cur_zorder2)
+
+            zvals = [np.min(pos[2, 0 : zsize + 1]), np.max(pos[2, 0 : zsize + 1])]
+            for j in range(ysize + 1):
+                yvals = pos[1, j] * np.array([1.0, 1.0])
+                ax.plot(zvals, yvals, cur_ptype2, alpha=cur_alpha, zorder=cur_zorder2)
 
         if parent_grid:
             ax.set_xlim([np.min(pos[2, 0 : zsize + 1]), np.max(pos[2, 0 : zsize + 1])])
             ax.set_ylim([np.min(pos[1, 0 : ysize + 1]), np.max(pos[1, 0 : ysize + 1])])
             ax.set_xlabel("z [pc]")
-            ax.set_title(f"x = {xval:.2f} pc")
+            ktitle = rf"x = {xval:.2f} pc"
+            if xval2 != xval:
+                ktitle = f"{ktitle}; x2 = {xval2:.2f} pc"
+            ax.set_title(ktitle)
             parent_grid = False
-            cur_ptype = "k-"
-            cur_alpha = 0.5
+            cur_ptype = "b-"
+            cur_alpha = 0.4
+            cur_zorder = 1
 
     fig.tight_layout()
 
