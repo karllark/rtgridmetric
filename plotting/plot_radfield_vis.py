1e5997b637c2e2e1cb75a43b70f9f79909855c07
Karl Gordon <kgordon@stsci.edu>
2025-10-29 10:39:56 -0400

    updating frac change visualization

--- plotting/plot_radfield_vis.py
+++ plotting/plot_radfield_vis.py
@@ -9,19 +9,21 @@ from astropy.io import fits
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", help="file name with radiation field")
+    parser.add_argument("basename", help="base file name with radiation field")
+    parser.add_argument("--xval", default=0.0, type=float, help="x slice value")
+    parser.add_argument("--zval", default=-3.5, type=float, help="z slice value")
     parser.add_argument("--png", help="save figure as a png file", action="store_true")
     parser.add_argument("--pdf", help="save figure as a pdf file", action="store_true")
     args = parser.parse_args()
 
-    fname = args.filename
-    rd = fits.getdata(fname)
+    rd = fits.getdata(f"{args.basename}_rad_field.fits")
+    pos = fits.getdata(f"{args.basename}_pos.fits")
     # remove the last big x cell that has no dust, just the star
     rd = rd[0, 0:-1, :, :]
 
-    fname_unc = fname.replace(".fits", "_unc.fits")
+    fname_unc = f"{args.basename}_rad_field_unc.fits"
     if os.path.isfile(fname_unc):
-        unc = fits.getdata(fname.replace(".fits", "_unc.fits"))
+        unc = fits.getdata(fname_unc)
         unc = unc[0, 0:-1, :, :]
     else:
         unc = None
@@ -40,32 +42,35 @@ if __name__ == "__main__":
 
     fig = plt.figure(layout="constrained", figsize=figsize)
 
-    gs = GridSpec(2, 2, figure=fig, width_ratios=[10, 3], height_ratios=[1, 1.5])
-    ax1 = fig.add_subplot(gs[0, :])
-    ax2 = fig.add_subplot(gs[1, 0])
-    ax3 = fig.add_subplot(gs[1, 1])
+    gs = GridSpec(2, 2, figure=fig, width_ratios=[10, 3], height_ratios=[1.5, 1])
+    ax1 = fig.add_subplot(gs[1, :])
+    ax2 = fig.add_subplot(gs[0, 0])
+    ax3 = fig.add_subplot(gs[0, 1])
     # ax4 = fig.add_subplot(gs[1, 2])
 
-    vmin = np.min(rd)
-    vmax = np.max(rd)
+    # vmin = np.min(rd)
+    # vmax = np.max(rd)
+    # cmap = "tab20b"
+    cmap = "afmhot"
+    # cmap = "cubehelix"
 
-    zslice = rd[6, :, :]
+    zidxs = np.argsort(np.absolute(pos[2, 0 : rd.shape[0]] - args.zval))
+    zslice = rd[zidxs[0], :, :]
     ax2.set_xlim(-5.0, 5.0)
     ax2.set_xlabel("x [pc]")
     ax2.set_ylim(-5.0, 5.0)
     ax2.set_ylabel("y [pc]")
-    zpos = ax2.imshow(
-        zslice, norm=LogNorm(vmin=vmin, vmax=vmax), aspect="auto", extent=ax2.axis()
-    )
+    zpos = ax2.imshow(zslice, aspect="auto", extent=ax2.axis(), origin="lower", cmap=cmap)
+    ax2.set_title(f"z = {pos[2, zidxs[0]]:.2f} pc")
 
-    xslice = np.transpose(rd[:, 5, :])
+    xidxs = np.argsort(np.absolute(pos[0, 0 : rd.shape[2]] - args.xval))
+    xslice = np.transpose(rd[:, xidxs[0], :])
     ax3.set_xlim(-5.0, -2.0)
     ax3.set_xlabel("z [pc]")
     ax3.set_ylim(-5.0, 5.0)
     # ax3.set_ylabel("y [pc]")
-    xpos = ax3.imshow(
-        xslice, norm=LogNorm(vmin=vmin, vmax=vmax), aspect="auto", extent=ax3.axis()
-    )
+    xpos = ax3.imshow(xslice, aspect="auto", extent=ax3.axis(), origin="lower", cmap=cmap)
+    ax3.set_title(f"x = {pos[0, xidxs[0]]:.2f} pc")
 
     ax = ax1
 
@@ -139,11 +144,10 @@ if __name__ == "__main__":
 
     ax1.set_xlabel("fractional change between cells")
     ax1.set_ylabel("# cells")
-    # ax1.set_title(fname)
 
     ax1.legend()
 
-    save_str = fname.replace(".fits", "_radfield_diffs")
+    save_str = f"{args.basename}_radfield_diffs_x{args.xval:.2f}_z{args.zval:.2f}"
     if args.png:
         fig.savefig(f"{save_str}.png")
     elif args.pdf:
--- regrid_uniform.py
+++ regrid_uniform.py
@@ -6,44 +6,88 @@ from astropy.io import fits
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", help="file name with radiation field")
+    parser.add_argument("basename", help="base file name with radiation field")
     args = parser.parse_args()
 
-    fname = args.filename
+    fname = f"{args.basename}_rad_field.fits"
     hdul = fits.open(fname)
 
     subsize = np.full(3, 1)
     for chdu in hdul[1:]:
         for l in range(3):
-            if chdu.data.shape[l+1] > subsize[l]:
-                subsize[l] = chdu.data.shape[l+1]
+            if chdu.data.shape[l + 1] > subsize[l]:
+                subsize[l] = chdu.data.shape[l + 1]
 
     # create a new 3D grid that has enough pixels in all dim
     mdata = hdul[0].data[:, 0:-1, :, :]
-    nrd = np.zeros((1,
-                    mdata.shape[1]*subsize[0],
-                    mdata.shape[2]*subsize[1],
-                    mdata.shape[3]*subsize[2]))
+    nrd = np.zeros(
+        (
+            1,
+            mdata.shape[1] * subsize[0],
+            mdata.shape[2] * subsize[1],
+            mdata.shape[3] * subsize[2],
+        )
+    )
+
+    # also write the position files so that the visualizations work
+    pos = fits.getdata(f"{args.basename}_pos.fits", 0)
+    newpos = np.zeros(
+        (
+            3,
+            max(
+                mdata.shape[1] * subsize[0] + 1,
+                mdata.shape[2] * subsize[1] + 1,
+                mdata.shape[3] * subsize[2] + 1,
+            ),
+        )
+    )
+    for i in range(3):
+        nold = hdul[0].data.shape[3 - i]
+        nnew = nrd.shape[3 - i] + 1
+        if i == 2:  # hack to handle not using the large empty z bins at the model edge
+            nold -= 1
+        newpos[i, 0:nnew] = np.linspace(
+            pos[i, 0], pos[i, nold], nnew
+        )
+
+    fits.writeto(f"{args.basename}_uniform_pos.fits", newpos, overwrite=True)
+    newpos2 = newpos * 0.0
+    newpos2[0, :] = newpos[2, :]
+    newpos2[1, :] = newpos[1, :]
+    newpos2[2, :] = newpos[0, :]
+    fits.writeto(f"{args.basename}_uniform_transpose_pos.fits", newpos2, overwrite=True)
 
     # get the subgrids and insert them into new grid
     for i in range(mdata.shape[1]):
         print("nz:", i)
-        nx = np.linspace(i*subsize[0], (i+1)*subsize[0]-1, subsize[0], dtype=int)
+        nx = np.linspace(
+            i * subsize[0], (i + 1) * subsize[0] - 1, subsize[0], dtype=int
+        )
         for j in range(mdata.shape[2]):
-            ny = np.linspace(j*subsize[1], (j+1)*subsize[1]-1, subsize[1], dtype=int)
+            ny = np.linspace(
+                j * subsize[1], (j + 1) * subsize[1] - 1, subsize[1], dtype=int
+            )
             for k in range(mdata.shape[3]):
-                nz = np.linspace(k*subsize[2], (k+1)*subsize[2]-1, subsize[2], dtype=int)
+                nz = np.linspace(
+                    k * subsize[2], (k + 1) * subsize[2] - 1, subsize[2], dtype=int
+                )
                 if mdata[0, i, j, k] < 0:
-                    chdu = hdul[int(-1*mdata[0, i, j, k])]
+                    chdu = hdul[int(-1 * mdata[0, i, j, k])]
                     ishape = np.array(chdu.shape[1:])
                     cdata = chdu.data[0, :, :, :]
                     for l in range(3):
                         if ishape[l] <= 1:
                             ishape[l] += 1
                             cdata = np.repeat(cdata, 2, axis=l)
-                    cx = np.linspace(i*subsize[0], ((i+1)*subsize[0])-1, ishape[0])
-                    cy = np.linspace(j*subsize[1], ((j+1)*subsize[1])-1, ishape[1])
-                    cz = np.linspace(k*subsize[2], ((k+1)*subsize[2])-1, ishape[2])
+                    cx = np.linspace(
+                        i * subsize[0], ((i + 1) * subsize[0]) - 1, ishape[0]
+                    )
+                    cy = np.linspace(
+                        j * subsize[1], ((j + 1) * subsize[1]) - 1, ishape[1]
+                    )
+                    cz = np.linspace(
+                        k * subsize[2], ((k + 1) * subsize[2]) - 1, ishape[2]
+                    )
                     interp = RegularGridInterpolator((cx, cy, cz), cdata)
                     for cnx in nx:
                         for cny in ny:
@@ -52,5 +96,13 @@ if __name__ == "__main__":
 
     hdul.close()
 
-    fits.writeto(args.filename.replace(".fits", "_uniform.fits"), nrd, overwrite=True)
-    fits.writeto(args.filename.replace(".fits", "_uniform_transpose.fits"), np.transpose(nrd[0, :, : :]), overwrite=True)
+    fits.writeto(f"{args.basename}_uniform_rad_field.fits", nrd, overwrite=True)
+    fits.writeto(
+        f"{args.basename}_uniform_transpose_rad_field.fits",
+        np.transpose(nrd[0, :, ::]),
+        overwrite=True,
+    )
+
+    # also write the position files so that the visualizations work
+    pos = fits.getdata(f"{args.basename}_pos.fits", 0)
+    print(pos.shape)
